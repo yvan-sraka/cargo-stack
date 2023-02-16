d14a6ba9c79f9e33f819fb650f4fb3defe57be87
Yvan Sraka <yvan@sraka.xyz>
2023-02-16 14:16:39 +0100

    v0.1.1
    - Add preliminary Haskell Tool Stack support
      https://github.com/yvan-sraka/cargo-cabal/pull/5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if let Err(e) = lib::parse_cli_args(args) {
+        println!("{}{}", ansi_term::Colour::Red.bold().paint("Error: "), e);
+    }
 }
